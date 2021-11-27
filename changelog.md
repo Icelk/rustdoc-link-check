9a292094f15f1b146ed9572276d54ed0c9474a99
Icelk <main@icelk.dev>
2021-11-27 13:28:11 +0100

    Compiling and Documenting output is now redirected to stdout directly.

---
+++ changelog.md
@@ -0,0 +1,10 @@
+# v1
+
+## v1.0.1
+
+The output while building and documenting are now shown.
+This is good for larger projects, which have to build dependencies to check links.
+
+## v1.0.0
+
+Initial release.
--- index.js
+++ index.js
@@ -3,22 +3,47 @@ const util = require("util")
 const childProcess = require("child_process")
 const execFile = util.promisify(childProcess.execFile)
 
+function awaitEvent(emitter, ev) {
+    return new Promise((resolve, _) => {
+        emitter.on(ev, () => resolve())
+    })
+}
+
 async function main() {
     let checkPrivate = core.getInput("private")
     if (checkPrivate != "true" && checkPrivate != "false") {
         core.setFailed("private must either be true or false")
+        process.exit(1)
     }
 
-    let args = ["doc", "--no-deps", "--quiet"]
+    let args = ["doc", "--no-deps", "--color", "always"]
     if (checkPrivate) {
         args.push("--document-private-items")
     }
-    let result = await execFile("cargo", args, { cwd: core.getInput("working-directory") }).catch((test) => {
-        return test
+    let child = childProcess.spawn("cargo", args, { cwd: core.getInput("working-directory") })
+
+    let output = ""
+    child.stderr.on("data", (data) => {
+        data = data.toString()
+        let lines = data.split("\n")
+        lines.forEach((line, index) => {
+            // Escape code for the green-ish colour
+            // If the line is ok, print it now.
+            // Else, add it to the stderr output.
+            if (line.startsWith("\x1B[0m\x1B[0m\x1B[1m\x1B[32m")) {
+                console.log(line)
+            } else {
+                output += line + (index + 1 == lines.length ? "" : "\n")
+            }
+        })
     })
-    let stderr = result.stderr
+    await awaitEvent(child, "close")
+
+    let stderr = output
 
     if (stderr.trim().length != 0) {
+        // Add a line of padding.
+        console.log()
         console.error(stderr)
         if (stderr.indexOf("could not find `Cargo.toml`") >= 0) {
             core.setFailed("This isn't a Cargo project.")
